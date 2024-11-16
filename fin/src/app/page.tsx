c35442ba7282aeed0f10d904e943e4076c765269
Ckayz <laicheongkian@gmail.com>
2024-11-16 22:10:24 +0700

    updated page

--- fin/src/app/page.tsx
+++ fin/src/app/page.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import LandingPage from "./landingPage/landing_page";
-import Test from "./landingTest/test";
-import FetchCreditScore from "./landingScore/score";
-import AssessmentCard from "./Search(without Login)/Search_page";
-import ParticlesComponent from "../../components/particles";
+import AssessmentCard from "./Search(without Login)/page";
+
 
 export default function Page() {
   return (
     <>
       
       <AssessmentCard />
-      // return <LandingPage />;
-      // return <Test />;
-      // return <FetchCreditScore/>;
     </>
   );
 }
