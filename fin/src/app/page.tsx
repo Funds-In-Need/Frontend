d7e0f32f291a4b1d78fb26cffc9ce2ffdd33f737
Wiki <gohweiqi1028@gmail.com>
2024-11-17 01:43:42 +0800

    update

--- fin/src/app/page.tsx
+++ fin/src/app/page.tsx
@@ -3,14 +3,15 @@ import LandingPage from "./landingPage/page";
 import Test from "./landingTest/test";
 import FetchCreditScore from "./landingScore/score";
 import AssessmentCard from "./Search(without Login)/page";
-import Search_Login from "./Search_Login/searchLogin";
 import ParticlesComponent from "../../components/particles";
 
 export default function Page() {
   return (
     <>
       <ParticlesComponent id="particles" />
-      <Search_Login />
+      <AssessmentCard />
+      {/* <LandingPage />; */}
+      <Test />;{/* <FetchCreditScore/>; */}
     </>
   );
 }
